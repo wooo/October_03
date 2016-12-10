52162cdbf24a4cfce9f145665a3c335ab2461cba
wooo <fotoz01@gmail.com>
2016-12-09 22:02:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas (600,400)
+    background (250, 250, 100)
+}
+
+function draw() {
+
+noStroke ()
+    fill (250, 200,200,50)
+  ellipse (mouseX,mouseY,50,50)
+    fill (200, 250,200)
+  rect (400, 100, 50,50)
+  
+}
+
+function mousePressed() {
+      background (250, 250, 100)
+}
